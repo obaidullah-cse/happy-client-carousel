a462fbdf7ca4c01ba9ed52b8f203223b48dfb99a
“obaidullah-cse” <cse.obaidullah@gmailcom>
2020-11-27 11:38:58 +0600

    carousel

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:1,
+        margin:10,
+        loop:true,
+        autoplay:true,
+        autoplayTimeout:4000,
+        dots:true,
+    });
+  });
